36e4c1071baf80329d4c3cf0af877b712e723542
Florian Richter <flori@hey.com>
2023-12-01 16:05:33 +0100

    Add solution for day 1

---
+++ src/1.ts
@@ -0,0 +1,54 @@
+import { AocClient } from "advent-of-code-client";
+import { PartFn } from "advent-of-code-client/dist/AocClient.types";
+
+const client = new AocClient({
+  year: 2023,
+  day: 1,
+  token: process.env.AOC_SESSION!,
+});
+
+const re = /^[^0-9]*?([0-9]).*?([0-9])?[^0-9]*?$/;
+
+function part1(input: string) {
+  return input
+    .split("\n")
+    .map((line) => re.exec(line))
+    .reduce(
+      (prev, match) =>
+        prev + parseInt(match![1]) * 10 + parseInt(match![2] ?? match![1]),
+      0
+    );
+}
+
+function replaceDigits(line: string) {
+  return line
+    .replace("one", "1")
+    .replace("two", "2")
+    .replace("three", "3")
+    .replace("four", "4")
+    .replace("five", "5")
+    .replace("six", "6")
+    .replace("seven", "7")
+    .replace("eight", "8")
+    .replace("nine", "9");
+}
+
+const re2 =
+  /([0-9]|one|two|three|four|five|six|seven|eight|nine).*([0-9]|one|two|three|four|five|six|seven|eight|nine)/;
+
+function part2(input: string) {
+  return input
+    .split("\n")
+    .map((line) => re2.exec(line) ?? re.exec(line))
+    .reduce(
+      (prev, match) =>
+        prev +
+        parseInt(
+          replaceDigits(match![1]) + replaceDigits(match![2] ?? match![1]),
+          10
+        ),
+      0
+    );
+}
+
+await client.run([part1 as PartFn, part2 as PartFn], false);
