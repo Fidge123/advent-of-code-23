d1be958e0a43f3f33d59e79dc51a8c43469dfba4
Florian Richter <flori@hey.com>
2023-12-02 11:19:25 +0100

    Remove example for day 2

--- src/02.ts
+++ src/02.ts
@@ -49,11 +49,4 @@ function part2(input: string) {
     .reduce((power, game) => power + game.blue * game.green * game.red, 0);
 }
 
-console.log(
-  part2(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`)
-);
 await client.run([part1 as PartFn, part2 as PartFn], false);
