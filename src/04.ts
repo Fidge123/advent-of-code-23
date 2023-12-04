ee1925f1ce096ae2a69c75a7a5662a2f78eb5504
Florian Richter <flori@hey.com>
2023-12-04 09:06:12 +0100

    Add solution for day 4

--- src/04.ts
+++ src/04.ts
@@ -8,11 +8,37 @@ const client = new AocClient({
 });
 
 function part1(input: string) {
-  return input;
+  return input
+    .split("\n")
+    .map((row) =>
+      row
+        .split(": ")[1]
+        .split(" | ")
+        .map((a) => a.trim().split(/  ?/))
+    )
+    .map(([wins, bets]) => bets.filter((bet) => wins.includes(bet)).length)
+    .reduce((sum, game) => (game > 0 ? sum + Math.pow(2, game - 1) : sum), 0);
 }
 
 function part2(input: string) {
-  return input;
+  const games = input
+    .split("\n")
+    .map((row) =>
+      row
+        .split(": ")[1]
+        .split(" | ")
+        .map((a) => a.trim().split(/  ?/))
+    )
+    .map(([wins, bets]) => bets.filter((bet) => wins.includes(bet)).length);
+  const copies = games.map(() => 1);
+
+  for (let i = 0; i < games.length; i++) {
+    for (let j = 1; j <= games[i] && i + j < copies.length; j++) {
+      copies[i + j] += copies[i];
+    }
+  }
+
+  return copies.reduce((sum, cards) => sum + cards);
 }
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
