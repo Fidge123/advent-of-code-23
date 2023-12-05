d93c8159a346bee20924d22d3a4fafddb7ce9e26
Florian Richter <flori@hey.com>
2023-12-05 10:41:31 +0100

    Add solution for day 5

--- src/05.ts
+++ src/05.ts
@@ -7,12 +7,73 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
-function part1(input: string) {
+function applyMap(input: number, maps: number[][], reverse = false) {
+  for (const [dest, source, range] of maps) {
+    if (
+      input < (reverse ? source : dest) + range &&
+      input >= (reverse ? source : dest)
+    ) {
+      return (reverse ? dest : source) + input - source;
+    }
+  }
   return input;
 }
 
+function part1(input: string) {
+  const [rawSeeds, ...rest] = input.split("\n\n");
+  const maps = rest.map((rawMaps) =>
+    rawMaps
+      .trim()
+      .split("\n")
+      .slice(1)
+      .map((map) => map.split(" ").map((m) => parseInt(m)))
+  );
+  const seeds = rawSeeds
+    .trim()
+    .split(" ")
+    .slice(1)
+    .map((seed) => parseInt(seed))
+    .map((seed) => maps.reduce((input, map) => applyMap(input, map), seed));
+
+  return Math.min(...seeds);
+}
+
+function isInitial(input: number, seedRanges: number[]) {
+  for (let i = 0; i < seedRanges.length; i += 2) {
+    if (input >= seedRanges[i] && input < seedRanges[i] + seedRanges[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function part2(input: string) {
-  return input;
+  const [rawSeeds, ...rest] = input.split("\n\n");
+  const maps = rest
+    .map((rawMaps) =>
+      rawMaps
+        .trim()
+        .split("\n")
+        .slice(1)
+        .map((map) => map.split(" ").map((m) => parseInt(m)))
+    )
+    .reverse();
+  const seeds = rawSeeds
+    .trim()
+    .split(" ")
+    .slice(1)
+    .map((seed) => parseInt(seed));
+
+  for (let loc = 0; loc < 1_000_000_000; loc++) {
+    if (
+      isInitial(
+        maps.reduce((i, map) => applyMap(i, map, true), loc),
+        seeds
+      )
+    ) {
+      return loc;
+    }
+  }
 }
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
