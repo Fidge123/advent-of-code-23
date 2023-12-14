202a7763bd1fbcd1786c8e8c7d43e29879873a3a
Florian Richter <flori@hey.com>
2023-12-14 18:16:07 +0100

    Add solution for day 13

--- src/13.ts
+++ src/13.ts
@@ -7,12 +7,83 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
+function rotateMatrix(matrix: string[][]) {
+  const temp = matrix.map((pattern) => pattern.map((row) => row.split("")));
+  return temp.map((pattern) =>
+    pattern[0].map((_, i) =>
+      pattern
+        .map((row) => row[i])
+        .reverse()
+        .join("")
+    )
+  );
+}
+
+function findMirrorIndex(pattern: string[]): number {
+  for (let i = 1; i < pattern.length; i++) {
+    if (
+      pattern.slice(i).every((row, j) => j >= i || row === pattern[i - (j + 1)])
+    ) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+function countDifference(a: string, b: string) {
+  return a
+    .split("")
+    .reduce((diff, char, i) => (!b || char === b[i] ? diff : diff + 1), 0);
+}
+
+function findSmugdedMirrorIndex(pattern: string[], ignoreLine: number): number {
+  for (let i = 1; i < pattern.length; i++) {
+    if (
+      i !== ignoreLine &&
+      pattern
+        .slice(i)
+        .map((row, j) => countDifference(row, pattern[i - (j + 1)]))
+        .reduce((a, b) => a + b, 0) === 1
+    ) {
+      return i;
+    }
+  }
+  return 0;
+}
+
 function part1(input: string) {
-  return input;
+  const patterns = input.split("\n\n");
+  const patternsByRow = patterns.map((pattern) => pattern.split("\n"));
+  const patternsByCols = rotateMatrix(patternsByRow);
+  return (
+    patternsByRow
+      .map((pattern) => findMirrorIndex(pattern))
+      .reduce((sum, index) => sum + index, 0) *
+      100 +
+    patternsByCols
+      .map((pattern) => findMirrorIndex(pattern))
+      .reduce((sum, index) => sum + index, 0)
+  );
 }
 
 function part2(input: string) {
-  return input;
+  const patterns = input.split("\n\n");
+  const patternsByRow = patterns.map((pattern) => pattern.split("\n"));
+  const patternsByCols = rotateMatrix(patternsByRow);
+
+  return (
+    patternsByRow
+      .map((pattern) =>
+        findSmugdedMirrorIndex(pattern, findMirrorIndex(pattern))
+      )
+      .reduce((sum, index) => sum + index, 0) *
+      100 +
+    patternsByCols
+      .map((pattern) =>
+        findSmugdedMirrorIndex(pattern, findMirrorIndex(pattern))
+      )
+      .reduce((sum, index) => sum + index, 0)
+  );
 }
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
